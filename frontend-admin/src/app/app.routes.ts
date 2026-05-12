d67dbb0d986cfaa85000f202e1249b15c7dbeeba
LucTer33 <lucasteranariel@gmail.com>
2026-05-13 00:24:45 +0200

    reorganizacion de carpetas y archivos

--- frontend-admin/src/app/app.routes.ts
+++ frontend-admin/src/app/app.routes.ts
@@ -1,21 +1,18 @@
 import { Routes } from '@angular/router';
 // Fíjate bien en esta línea:
-import { LoginComponent } from './components/login/login';
-import { Dashboard } from './components/dashboard/dashboard';
-import { RutasSection} from './components/rutas-section/rutas-section';
-import { ZonasSection } from './components/zonas-section/zonas-section';
-import { ObjetosSection } from './components/objetos-section/objetos-section';
-import { UsuariosSection } from './components/usuarios-section/usuarios-section';
+import { LoginComponent } from './pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { RutasSection } from './pages/rutas-section/rutas-section';
+import { ZonasSection } from './pages/zonas-section/zonas-section';
+import { ObjetosSection } from './pages/objetos-section/objetos-section';
+import { UsuariosSection } from './pages/usuarios-section/usuarios-section';
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent },
-    { path: 'dashboard', component: Dashboard,
-        children: [
-            {path: 'rutas', component: RutasSection},
-            {path: 'zonas', component: ZonasSection},
-            {path: 'objetos', component: ObjetosSection},
-            {path: 'usuarios', component: UsuariosSection}
-        ]
-     },
-    { path: '**', redirectTo: '' }
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'dashboard', component: Dashboard },
+    { path: 'rutas', component: RutasSection },
+    { path: 'zonas', component: ZonasSection },
+    { path: 'objetos', component: ObjetosSection },
+    { path: 'usuarios', component: UsuariosSection },
 ];
